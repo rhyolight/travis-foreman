811fad0252c48f6fc26d603e4c0f5e15eae9c884
Matthew Taylor <rhyolight@gmail.com>
2014-06-05 13:27:57 -0700

    Initial commit.

---
+++ monitor.js
@@ -0,0 +1,127 @@
+var _ = require('underscore'),
+    async = require('async'),
+    Travis = require('travis-ci');
+
+function splitArguments(args) {
+    var token = undefined,
+        config = {},
+        callback = function() {};
+    _.each(args, function(arg) {
+        if (typeof(arg) == 'string') {
+            token = arg;
+        } else if (typeof(arg) == 'function') {
+            callback = arg;
+        } else {
+            config = arg;
+        }
+    });
+    return [token, config, callback];
+}
+
+function TravisBuildMonitor(config, authCallback) {
+    var cfg = _.extend({}, config),
+        callback = authCallback;
+    if (! callback) {
+        callback = function() {};
+    }
+    // Verify required configuration elements.
+    _.each(['organization', 'username', 'password'], function(configName) {
+        if (! cfg[configName]) {
+            throw new Error('Missing ' + configName + ' configuration.');
+        }
+    });
+    this.organization = cfg.organization;
+    this.username = cfg.username;
+    this.password = cfg.password;
+    this.travis = new Travis({ version: '2.0.0' });
+    this.travis.authenticate({
+        username: this.username,
+        password: this.password
+    }, callback);
+}
+
+TravisBuildMonitor.prototype.authenticate = function(callback) {
+    this.travis.authenticate({
+        username: this.username,
+        password: this.password
+    }, callback);
+};
+
+TravisBuildMonitor.prototype.listRepos = function(callback) {
+    this.travis.repos({
+        owner_name: this.organization
+    }, function(err, response) {
+        if (err) {
+            callback(err);
+        } else {
+            callback(err, response.repos);
+        }
+    });
+};
+
+TravisBuildMonitor.prototype.listBuilds = function() {
+    var me = this,
+        args = splitArguments(arguments),
+        repoName = args[0],
+        filter = args[1],
+        callback = args[2];
+    // If there is no repoName, user wants all the builds for all the repos.
+    if (! repoName) {
+        // First we need to get all the repos.
+        this.listRepos(function(err, repos) {
+            var repoBuildFunctions = {};
+            if (err) {
+                return callback(err);
+            }
+            // We'll execute the listBuilds function for each repo name in parallel.
+            _.each(_.map(repos, function(repo) {
+                return repo.slug.split('/').pop();
+            }), function(repoName) {
+                repoBuildFunctions[repoName] = function(localCallback) {
+                    me.listBuilds(repoName, filter, localCallback);
+                };
+            });
+            async.parallel(repoBuildFunctions, callback);
+        });
+    }
+    // Otherwise we just return the builds for the specified repo.
+    else {
+        this.travis.repos.builds({
+            owner_name: this.organization,
+            name: repoName
+        }, function(err, response) {
+            var filteredOutput = [];
+            if (err) {
+                return callback(err);
+            }
+            // Filter builds if necessary
+            if (_.size(filter) > 0) {
+                filteredOutput = _.compact(_.collect(response.builds, function(build) {
+                    var includedBuild = build;
+                    _.each(filter, function(filterValue, filterBy) {
+                        if (build[filterBy] != filterValue) {
+                            includedBuild = false;
+                        }
+                    });
+                    return includedBuild;
+                }));
+                callback(null, filteredOutput);
+            }
+            // If no filter, just return all builds.
+            else {
+                callback(null, response.builds);
+            }
+        });
+
+    }
+};
+
+TravisBuildMonitor.prototype.listRunningBuilds = function() {
+    var me = this,
+        args = splitArguments(arguments),
+        repoName = args[0],
+        callback = args[2];
+    this.listBuilds(repoName, {state: 'started'}, callback);
+};
+
+module.exports = TravisBuildMonitor;
---
+++ test/monitor-tests.js
@@ -0,0 +1,293 @@
+var assert = require('chai').assert,
+    expect = require('chai').expect,
+    should = require('chai').should(),
+    proxyquire = require('proxyquire'),
+    mockRepos = require('./mock-data/repo-list'),
+    mockNupicBuilds = require('./mock-data/nupic-build-list'),
+    mockNupicCoreBuilds = require('./mock-data/nupic-core-build-list');
+
+describe('build monitor', function() {
+
+    describe('when constructed', function() {
+        var authenticated = false;
+        var mockTravisInstance = {
+            authenticate: function(params, callback) {
+                assert.ok(params.username, 'Missing travis-ci username during authentication.');
+                assert.ok(params.password, 'Missing travis-ci password during authentication.');
+                authenticated = true;
+                callback();
+            }
+        };
+
+        var Monitor = proxyquire('../monitor', {
+            'travis-ci': function () {
+                return mockTravisInstance;
+            }
+        });
+
+        describe('without organization', function() {
+            it('throws proper error', function() {
+                expect(function() {
+                    new Monitor({
+                        username: 'my-username',
+                        password: 'my-password'
+                    });
+                }).to.throw('Missing organization configuration.');
+            });
+        });
+
+        describe('without username', function() {
+            it('throws proper error', function() {
+                expect(function() {
+                    new Monitor({
+                        organization: 'my-organization',
+                        password: 'my-password'
+                    });
+                }).to.throw('Missing username configuration.');
+            });
+        });
+
+        describe('without password', function() {
+            it('throws proper error', function() {
+                expect(function() {
+                    new Monitor({
+                        organization: 'my-organization',
+                        username: 'my-username'
+                    });
+                }).to.throw('Missing password configuration.');
+            });
+        });
+
+
+        describe('with required configuration', function() {
+
+            var monitor = new Monitor({
+                organization: 'my-organization',
+                username: 'my-username',
+                password: 'my-password'
+            });
+
+            it('exposes organization', function() {
+                assert.equal(monitor.organization, 'my-organization', 'Bad org monitor property.');
+            });
+
+            it('exposes username', function() {
+                assert.equal(monitor.username, 'my-username', 'Bad username monitor property.');
+            });
+
+            describe('during authentication', function() {
+                it('calls the given callback after authentication', function(done) {
+                    monitor.authenticate(function(err) {
+                        expect(err).to.not.exist;
+                        assert.isTrue(authenticated, 'Monitor did not authenticate with travis-ci.');
+                        done();
+                    });
+                });
+
+            });
+
+        });
+
+        describe('when created with bad organization', function() {
+            var mockTravisInstance = {
+                authenticate: function(params, cb) {
+                    if (cb) {
+                        cb();
+                    }
+                },
+                repos: function(params, callback) {
+                    callback(new Error('travis-ci error'));
+                }
+            };
+
+            var Monitor = proxyquire('../monitor', {
+                'travis-ci': function () {
+                    return mockTravisInstance;
+                }
+            });
+
+            var monitor = new Monitor({
+                organization: 'my-organization',
+                username: 'my-username',
+                password: 'my-password'
+            });
+
+            it('returns proper error when listing repos', function(done) {
+                monitor.listRepos(function(err, repos) {
+                    assert.ok(err, 'should have received error on repo list with bad org');
+                    assert.notOk(repos, 'should not have received data');
+                    done();
+                });
+            });
+
+        });
+    });
+
+    describe('with one repository', function() {
+
+        var authenticated = false;
+        var mockTravisInstance = {
+            authenticate: function(params, callback) {
+                expect(params).to.have.property('username').and.equal('my-username');
+                expect(params).to.have.property('password').and.equal('my-password');
+                authenticated = true;
+                callback();
+            },
+            repos: function(params, callback) {
+                expect(params).to.have.property('owner_name').and.equal('my-organization');
+                callback(null, mockRepos);
+            }
+        };
+
+        // Mock builds function.
+        mockTravisInstance.repos.builds = function(params, callback) {
+            expect(params).to.have.property('owner_name').and.equal('my-organization');
+            expect(params).to.have.property('name').and.equal('my-repo');
+            callback(null, mockNupicBuilds);
+        };
+
+        var Monitor = proxyquire('../monitor', {
+            'travis-ci': function () {
+                return mockTravisInstance;
+            }
+        });
+
+        var monitor = new Monitor({
+            organization: 'my-organization',
+            username: 'my-username',
+            password: 'my-password'
+        });
+
+
+        it('lists repo information for an organization', function(done) {
+            monitor.listRepos(function(err, repos) {
+                expect(err).to.not.exist;
+                expect(repos).to.be.instanceOf(Array);
+                expect(repos).to.have.length(9);
+                expect(repos[0]).to.be.an('object');
+                expect(repos[0]).to.have.property('id').and.equal(1839953);
+                done();
+            });
+        });
+
+        it('lists build information for an repo', function(done) {
+            monitor.listBuilds('my-repo', function(err, builds) {
+                expect(err).to.not.exist;
+                expect(builds).to.be.instanceOf(Array);
+                expect(builds).to.have.length(25);
+                expect(builds[0]).to.be.an('object');
+                expect(builds[0]).to.have.property('id').and.equal(26806886);
+                done();
+            });
+        });
+
+        it('lists running builds for on repo', function(done) {
+            monitor.listRunningBuilds('my-repo', function(err, builds) {
+                expect(err).to.not.exist;
+                expect(builds).to.be.instanceOf(Array);
+                expect(builds).to.have.length(2);
+                expect(builds[0]).to.be.an('object');
+                expect(builds[0]).to.have.property('id').and.equal(26806886);
+                done();
+            });
+        });
+
+    });
+
+    describe('with two repositories', function() {
+
+        var mockTravisInstance = {
+            authenticate: function(params, callback) {
+                callback();
+            },
+            repos: function(params, callback) {
+                expect(params).to.have.property('owner_name').and.equal('my-organization');
+                callback(null, mockRepos);
+            }
+        };
+
+        // Mock builds function.
+        mockTravisInstance.repos.builds = function(params, callback) {
+            expect(params).to.have.property('owner_name').and.equal('my-organization');
+            expect(params).to.have.property('name').and.equal('my-repo');
+            callback(null, mockNupicBuilds);
+        };
+
+        var Monitor = proxyquire('../monitor', {
+            'travis-ci': function () {
+                return mockTravisInstance;
+            }
+        });
+
+        var monitor = new Monitor({
+            organization: 'my-organization',
+            username: 'my-username',
+            password: 'my-password'
+        });
+
+        mockTravisInstance.repos.builds = function(params, callback) {
+            if (params.name == 'nupic') {
+                return callback(null, mockNupicBuilds);
+            } else if (params.name == 'nupic.core') {
+                callback(null, mockNupicCoreBuilds);
+            } else {
+                // dummy all others
+                callback(null, {builds: [], commits: []});
+            }
+        };
+
+        var Monitor = proxyquire('../monitor', {
+            'travis-ci': function () {
+                return mockTravisInstance;
+            }
+        });
+
+        var monitor = new Monitor({
+            organization: 'my-organization',
+            username: 'my-username',
+            password: 'my-password'
+        });
+
+        it('lists running builds for all repos', function(done) {
+            monitor.listRunningBuilds(function(err, builds) {
+                expect(err).to.not.exist;
+                expect(builds).to.be.instanceOf(Object);
+                expect(builds).to.include.keys('nupic');
+                expect(builds['nupic']).to.be.instanceOf(Array);
+                expect(builds['nupic']).to.have.length(2);
+                expect(builds['nupic'][0]).to.have.property('id').and.equal(26806886);
+                expect(builds).to.include.keys('nupic.core');
+                expect(builds['nupic.core']).to.be.instanceOf(Array);
+                expect(builds['nupic.core']).to.have.length(0);
+                done();
+            });
+        });
+
+        it('lists build information for many repos', function(done) {
+            monitor.listBuilds(function(err, builds) {
+                expect(err).to.not.exist;
+                expect(builds).to.be.instanceOf(Object);
+                expect(builds).to.have.property('nupic');
+                expect(builds).to.have.property('nupic.core');
+
+                var nupicBuilds = builds['nupic'],
+                    coreBuilds = builds['nupic.core'];
+
+                assert.isArray(nupicBuilds, 'wrong response type returned for build list');
+                assert.lengthOf(nupicBuilds, 25, 'wrong response list length');
+                assert.ok(nupicBuilds[0].id, 'Missing id of build');
+                assert.equal(nupicBuilds[0].id, 26806886, 'bad build id');
+
+                assert.isArray(coreBuilds, 'wrong response type returned for build list');
+                assert.lengthOf(coreBuilds, 25, 'wrong response list length');
+                assert.ok(coreBuilds[0].id, 'Missing id of build');
+                assert.equal(coreBuilds[0].id, 26783065, 'bad build id');
+
+                done();
+            });
+        });
+
+    });
+
+});
+
